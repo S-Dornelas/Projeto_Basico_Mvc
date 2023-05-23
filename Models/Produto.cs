d5fdd9debc031bc1e1e9a98125ed3fb3574aeb09
sandr <sandr@DORNELAS>
2023-05-23 15:08:44 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Produto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppMvcBasica.Models
+{
+    public class Produto : Entity
+    {
+        public Guid FornecedorId { get; set; }
+
+        [Required(ErrorMessage ="O campo {0} é obrigatório")] //O parametro {0} é a propriédade
+        [StringLength(200, MinimumLength = 2, ErrorMessage = "O campo {0} precisar ter entre {2} e {1} caracteres.")]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(1000, MinimumLength = 2, ErrorMessage = "O campo {0} precisar ter entre {2} e {1} caracteres.")]
+        public string? Descricao { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(200, MinimumLength = 2, ErrorMessage = "O campo {0} precisar ter entre {2} e {1} caracteres.")]
+        public string? Imagem { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public decimal Valor { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+
+        public Fornecedor? Fornecedor { get; set; }
+    }
+}
